a150e4e7a67d0ed9d4dedd3b791153239c64f974
ASAD <asadullah5368@gmail.com>
2024-11-13 11:10:24 +0500

    Added Required Packages

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 import express from 'express'
 
-const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
