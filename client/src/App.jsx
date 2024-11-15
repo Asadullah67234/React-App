cf3f4f7395e93d575e4d1f0c109b5fe32b066126
ASAD <asadullah5368@gmail.com>
2024-11-15 18:10:48 +0500

    Created Pages And Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Product from './Pages/Product'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 
 const App = () => {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/products' element={<Product/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
+    </Routes>
     </>
   )
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex items-center bg-slate-500  p-3 justify-between ">
+        <h1 className="font-bold text-2xl cursor-pointer">COMPANY NAME</h1>
+        <ul className="flex items-center gap-10">
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </ul>
+        <div className="flex gap-5">
+          <button className="cursor-pointer border-2 border-red-500 py-2 px-3 rounded-lg hover:bg-red-500">Log In</button>
+          <button className="cursor-pointer border-2 border-red-500 py-2 px-3 rounded-lg hover:bg-red-500">Sign Up</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
